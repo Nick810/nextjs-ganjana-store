6b5ba7dbac249c87bf6f0035bc72c5e4a9f90079
N1ck <cbnickp@gmail.com>
2023-05-02 14:25:16 +0700

    add gtm

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,15 @@
 import '@/styles/globals.css'
 import { AppContextProvider } from '../context';
 import Layout from '../components/layout';
+import TagManager from 'react-gtm-module';
 import "@fontsource/jost/700.css";
 import "@fontsource/jost/500.css";
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-KZG96FS' })
+  }, [])
   return (
     <AppContextProvider>
       <Layout data-theme="black">
